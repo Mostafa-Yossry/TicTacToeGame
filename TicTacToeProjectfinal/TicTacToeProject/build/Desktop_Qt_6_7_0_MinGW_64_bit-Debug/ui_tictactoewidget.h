33a0089e098a4a9606ca44b7239393f090a5e155
Mostafa-Yossry <146856433+Mostafa-Yossry@users.noreply.github.com>
2024-06-27 08:18:15 +0300

    Add files via upload

---
+++ TicTacToeProjectfinal/TicTacToeProject/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/ui_tictactoewidget.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'tictactoewidget.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TICTACTOEWIDGET_H
+#define UI_TICTACTOEWIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_TicTacToeWidget
+{
+public:
+
+    void setupUi(QWidget *TicTacToeWidget)
+    {
+        if (TicTacToeWidget->objectName().isEmpty())
+            TicTacToeWidget->setObjectName("TicTacToeWidget");
+        TicTacToeWidget->resize(800, 600);
+
+        retranslateUi(TicTacToeWidget);
+
+        QMetaObject::connectSlotsByName(TicTacToeWidget);
+    } // setupUi
+
+    void retranslateUi(QWidget *TicTacToeWidget)
+    {
+        TicTacToeWidget->setWindowTitle(QCoreApplication::translate("TicTacToeWidget", "TicTacToeWidget", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class TicTacToeWidget: public Ui_TicTacToeWidget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TICTACTOEWIDGET_H
---
+++ TicTacToeProjectfinal/TicTacToeProject/test_sessionmanager.h
@@ -0,0 +1,18 @@
+#ifndef TEST_SESSIONMANAGER_H
+#define TEST_SESSIONMANAGER_H
+
+#include <QObject>
+#include <QtTest/QtTest>
+#include "sessionmanager.h"
+
+class TestSessionManager : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testLogin();
+    void testLogout();
+    void testGetCurrentUser();
+};
+
+#endif // TEST_SESSIONMANAGER_H
